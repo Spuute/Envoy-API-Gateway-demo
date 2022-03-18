08db145f4dd008029f79a0b1b8b031e9e965714b
Spuute <spuute@gmail.com>
2022-03-18 22:44:46 +0100

    feat: adds dockerfile

--- TeaAPI/obj/Debug/net6.0/TeaAPI.AssemblyInfo.cs
+++ TeaAPI/obj/Debug/net6.0/TeaAPI.AssemblyInfo.cs
@@ -10,6 +10,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("Tea-API-afd2a36a-60ca-40fd-896d-cd0bf33e2953")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("TeaAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
